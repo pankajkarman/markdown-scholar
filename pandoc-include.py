a3fc744a11a41adf0921a474b841ec28d526e16f
pankajkarman <34066356+pankajkarman@users.noreply.github.com>
2019-07-30 00:21:39 +0530

    Add files via upload

--- pandoc-include.py
+++ pandoc-include.py
@@ -1,19 +1,5 @@
 import re, argparse
-
-def replace_include(infile, outfile):
-    with open(infile, 'r') as data:
-        lines = data.readlines()
-        for line in lines:
-            if re.search("!include", line):
-                idx = lines.index(line)
-                extfile = line.split(' ')[-1].split('\n')[0]
-                with open(extfile, 'r') as ext:
-                    content = ext.readlines()
-                content = ' '.join(content)
-    lines[idx] =  content
-    lines = ''.join(lines)
-    with open(outfile, 'w') as newfile:
-        newfile.write(lines)
+from pandoc import replace_include
 
 parser = argparse.ArgumentParser(description='Pandoc filter for include statement')
 parser.add_argument('--infile', help='Give input filename.')
